b71e701ca24185c6c77e1ef2c9c1616b88d3639d
David Yanez <david.yanez@sap.com>
2022-10-06 16:47:54 +0000

    30

--- app/resources/fiori/webapp/Component.js
+++ app/resources/fiori/webapp/Component.js
@@ -7,3 +7,13 @@ sap.ui.define(['sap/fe/core/AppComponent'], function (AppComponent) {
         }
     });
 });
+
+import { AlertNotificationClient, RegionUtils, BasicAuthentication } from '@sap_oss/alert-notification-client';
+
+const client = new AlertNotificationClient({
+  authentication: new BasicAuthentication({
+    username: '<your-technical-client-username>', // Replace with your username
+    password: '<your-technical-client-password>' // Replace with your password
+  }),
+  region: RegionUtils.EU10-004 // Choose your region
+});
